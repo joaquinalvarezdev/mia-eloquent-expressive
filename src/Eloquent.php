a32b7e2c35bf024bcfec364d77483f9d38389ab8
Matias Camiletti <matias.camiletti@gmail.com>
2018-06-15 23:21:10 -0300

    Fix namespace.

--- src/Eloquent.php
+++ src/Eloquent.php
@@ -7,7 +7,7 @@ class Eloquent
      */
     public static function install(\Psr\Container\ContainerInterface $container)
     {
-        $capsule = new Illuminate\Database\Capsule\Manager();
+        $capsule = new \Illuminate\Database\Capsule\Manager();
         $capsule->addConnection($container->get('config')['eloquent']);
         $capsule->setAsGlobal();
         $capsule->bootEloquent();
