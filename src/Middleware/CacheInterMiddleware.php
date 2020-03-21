0b3bfac3961ba14d4369781b29312efe82212c3c
Matias Camiletti <matias.camiletti@gmail.com>
2020-03-21 09:36:17 -0300

    Mejorado middleware de cache para verificar los parametros.

--- src/Middleware/CacheInterMiddleware.php
+++ src/Middleware/CacheInterMiddleware.php
@@ -12,14 +12,22 @@ class CacheInterMiddleware extends \Mobileia\Expressive\Middleware\MiaBaseMiddle
      * @var string
      */
     protected $cacheKey = '';
+    /**
+     *
+     * @var array
+     */
+    protected $withParams = [];
     
-    public function __construct($cacheKey)
+    public function __construct($cacheKey, $withParams = [])
     {
         $this->cacheKey = $cacheKey;
+        $this->withParams = $withParams;
     }
     
     public function process(\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Server\RequestHandlerInterface $handler): \Psr\Http\Message\ResponseInterface
     {
+        // Procesar parametros
+        $this->processParams($request);
         // Verificar si existe en cache
         $item = \Mobileia\Expressive\Database\Repository\CacheInterRepository::get($this->cacheKey);
         if($item !== null){
@@ -27,5 +35,20 @@ class CacheInterMiddleware extends \Mobileia\Expressive\Middleware\MiaBaseMiddle
         }
         // Seguimos el flujo
         return $handler->handle($request);
+    }
+    
+    protected function processParams(\Psr\Http\Message\ServerRequestInterface $request)
+    {
+        if(count($this->withParams) == 0){
+            return;
+        }
+        
+        // Recorremos cada parametro
+        foreach($this->withParams as $keyParam){
+            // Obtenemos valor del parametro
+            $paramVal = $this->getParam($request, $keyParam, '');
+            // Agregamos este parametro a la key
+            $this->cacheKey .= '_' . $paramVal;
+        }
     }
 }
